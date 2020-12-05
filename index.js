01d4ca3e8b50b56ff244a5fa70542c630338d6d7
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2020-12-05 13:35:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log(dodger.style.left); // "180px"
 console.log(dodger.style.bottom); // "0px"
 dodger.style.bottom = "100px";
 dodger.style.bottom = "0px";
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+});
